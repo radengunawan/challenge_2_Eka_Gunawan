8a06d753e707d30d62893d3e3ad93d54662d0b87
Eka Gunawan <send.raden@gmail.com>
2025-07-31 22:16:31 +0700

    attempt 1 - header

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+export default {
+  content: ['./src/**/*.{html,js}'], // Adjust this if your HTML is elsewhere
+  darkMode: 'class', // Enables class-based dark mode
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
